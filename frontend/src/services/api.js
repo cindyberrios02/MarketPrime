02973834031c84631de5684c9e51fa9b5cf3b2c0
Cindy Berrios <cindy.berrios02@gmail.com>
2026-05-23 17:41:19 -0400

    Corregir duplicacion de ruta api en Axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://marketprime-api-c4e6h4ath5dzhcat.westus3-01.azurewebsites.net/api';
+const API_BASE_URL = 'https://marketprime-api-c4e6h4ath5dzhcat.westus3-01.azurewebsites.net';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
